f54f5f6729efa732853965d2ea91f72bd1e54a03
saeta <saeta@cs.stanford.edu>
2011-05-10 23:10:54 -0700

    Added some models stuff.

--- Superlatives/survey/models.py
+++ Superlatives/survey/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+GENDER = (
+  ('m', 'Male'),
+  ('f', 'Female'),
+)
+
+YEAR = (
+  ('f', 'Frosh'),
+  ('s', 'Sophomore'),
+  ('j', 'Junior'),
+  ('r', 'Senior'),
+) 
+
+
+class Resident(models.Model):
+  name = models.CharField(max_length=200)
+  gender = models.CharField(max_length=1, choices=GENDER)
+  year = models.CharField(max_length=1, choices=YEAR)
+  staff = models.BooleanField()
+
+
