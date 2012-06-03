0cfcea4622b5acd694bc8083bd6c62cf781790f1
Zahan Malkani <zahanm@gmail.com>
2012-06-02 18:29:59 -0700

    separate fixtures, select with WHERE

--- scripts/userextract.js
+++ scripts/userextract.js
@@ -1,15 +1,24 @@
 
-var fs, connection;
+var fs, connection, residents, fields, dorm, dump;
 
 fs = require('fs');
 
 connection = require('mysql').createConnection( require('./dbauth') );
 
+residents = {};
+fields = ['sunetid', 'first_name', 'last_name', 'gender'];
+dorm = 'Cardenal';
+dump = fs.createWriteStream('people.json');
+
 connection.connect();
 
-connection.query('SELECT * FROM roster LIMIT 2', function(err, rows, fields) {
+connection.query(
+'SELECT '+ fields.join(', ') + ' FROM roster WHERE dorm = ? LIMIT 2',
+[ dorm ],
+function(err, rows, fields) {
   if (err) { console.error(err); return; }
   console.log('Result:', rows);
+  dump.end();
 });
 
 connection.end();
