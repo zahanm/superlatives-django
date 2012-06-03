43c9cbd17e83ddaeffeddb813bca6a3137854595
Zahan Malkani <zahanm@gmail.com>
2012-06-02 17:20:25 -0700

    remove cruft from settings

--- superlatives/settings.py
+++ superlatives/settings.py
@@ -12,20 +12,12 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-if True:
-  DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-  DATABASE_NAME = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'cardsuperdb.sqlite3')
-  DATABASE_USER = ''             # Not used with sqlite3.
-  DATABASE_PASSWORD = ''         # Not used with sqlite3.
-  DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-  DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
-else:
-  DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-  DATABASE_NAME = 'superlatives'             # Or path to database file if using sqlite3.
-  DATABASE_USER = 'www-data'             # Not used with sqlite3.
-#  DATABASE_PASSWORD = ''         # Not used with sqlite3.
-  DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-  DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'cardsuperdb.sqlite3')
+DATABASE_USER = ''             # Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
