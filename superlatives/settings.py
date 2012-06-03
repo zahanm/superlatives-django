e16af0b00b92d303d80e5b70f906a5fbea2fc28d
Zahan Malkani <zahanm@gmail.com>
2012-06-02 19:26:02 -0700

    make compatible with Django 1.4

--- superlatives/settings.py
+++ superlatives/settings.py
@@ -12,12 +12,12 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'cardsuperdb.sqlite3')
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': 'db/cardsuperdb.sqlite3'
+  }
+}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -55,8 +55,8 @@ SECRET_KEY = 'y8#(+4&$v_*u_m5cidi9q9ajcxx=9!*=%5dwi!m(ix0zo7u#z6'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
 #     'django.template.loaders.eggs.load_template_source',
 )
 
@@ -81,3 +81,6 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'survey',
 )
+
+# Family feud specific settings
+FEUD_DATE_TIME = '6pm, Saturday June 9'
