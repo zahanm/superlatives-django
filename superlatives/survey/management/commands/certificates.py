5aad75f963c899a52d076c5366d288fef55c196a
Zahan Malkani <zahanm@gmail.com>
2012-06-09 05:11:50 -0700

    certificates subcommand to gen certificates

---
+++ superlatives/survey/management/commands/certificates.py
@@ -0,0 +1,25 @@
+
+from pprint import PrettyPrinter
+
+from django.core.management.base import BaseCommand, CommandError
+
+from survey.models import Resident, Question, Answer
+from survey import feud
+
+class Command(BaseCommand):
+
+  help = 'Generates certificates for each resident'
+
+  def handle(self, *args, **options):
+    award_map = {}
+    for r in Resident.objects.all():
+      award_map[ r.name ] = []
+    for q in Question.objects.filter(istwoans=False):
+      result = feud.top_k_for_question(q, 1)
+      if len(result['answers']):
+        award_map[ result['answers'][0]['one'] ].append( q.qtext )
+    pp = PrettyPrinter(indent=2)
+    pp.pprint(award_map)
+
+if __name__ == '__main__':
+  associate_names()
